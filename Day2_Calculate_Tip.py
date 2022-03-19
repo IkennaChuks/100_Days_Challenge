8fe71c89d58b714dea5858ab575a01404ef1a637
ikennachuks <chuks2ikenna@gmail.com>
2022-03-19 19:13:36 +0100

    Day2 File was added

---
+++ Day2_Calculate_Tip.py
@@ -0,0 +1,8 @@
+print('welcome to the tip calculator.')
+bill = float(input('what was the total bill? $'))
+people = int(input('How many people to split the bill? '))
+tip = int(input('What percentage tip would you like to give? 10, 12, or 15? '))
+tip = (tip + 100)/100
+bill = bill * tip
+charge = bill / people
+print('Each person should pay $'+ str(charge))
