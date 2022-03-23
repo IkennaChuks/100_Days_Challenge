8c33e41d7ed2910fd5a263fb4a5a45ff8ca8be9a
ikennachuks <chuks2ikenna@gmail.com>
2022-03-23 19:20:26 +0100

    Added Coin game

---
+++ Day4_Coingame.py
@@ -0,0 +1,18 @@
+# Toss a coin to get head or tail. If coin is 0 then Tail else Head.
+
+import random
+
+coin = random.randint(0,1)
+
+if coin == 0:
+  print('Tail')
+else:
+  print('Head')
+
+
+
+
+
+
+
+
