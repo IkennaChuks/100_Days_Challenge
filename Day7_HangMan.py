dbcc27ed4b2d3b220a49bedb3af88cade9df97cf
ikennachuks <chuks2ikenna@gmail.com>
2022-04-09 16:03:01 +0200

    Added Hangman game

---
+++ Day7_HangMan.py
@@ -0,0 +1,102 @@
+#Step 2
+
+import random
+
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+
+
+Selected = []
+
+for letter in chosen_word:
+  Selected.append("_")
+#TODO-2: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+Live = 6
+while "_" in Selected and Live > 0:
+  
+  guess = input("Guess a letter: ").lower()
+  for position in range(len(chosen_word)):
+    alpha = chosen_word[position]
+    if alpha == guess:
+      Selected[position] = guess
+  if guess not in chosen_word:
+    Live = Live - 1 
+  
+  print(Selected)
+  print(stages[Live])
+if Live > 0:
+  print("You Won")  
+else:
+  print('You Lost')
+          
+
+
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
